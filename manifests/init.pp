3a9776f36fbb079f62b56d43769fa64edda35696
gafs01 <info@gafsad.com>
2018-03-12 17:17:15 +0100

    edit testrepo

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+class testrepo  {
+        if $environments == 'production' {
+           notify {'default-message' :
+}
+
+} else {
+       notify  {*default-message' :
+           message =>  "This is not production"
+
+  }
+
+   }
+}
